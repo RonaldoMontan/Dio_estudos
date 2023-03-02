877053d8a182445d2915e6c9adcb9391e87b0a5e
Ronaldo Montan <ronaldogithub@gmail.com>
2023-03-01 22:31:27 -0300

    iniciando segunda etapa adicionadno funções, ainda não esta pronto

--- Formação Python Developer/Criando um Sistema Bancário com Python/banco.py
+++ Formação Python Developer/Criando um Sistema Bancário com Python/banco.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+
+saldo = 0
 def choice():
     escolha = str(input("""
     _______Menu_______
@@ -12,51 +14,66 @@ def choice():
     >>""")).lower()[0]
     return escolha
 
-saldo = 0
+
+def sacar(saldo):
+    saldoatual = saldo_atual(0)
+    valor_s = int(input("Você pode sacar até R$500,00\n>>"))
+    while valor_s > 500:
+        valor_s = int(input(f"O valor R${valor_s:.2f} INVALIDO, maximo permitido 500,00\n>>"))
+
+    while valor_s > saldoatual:
+        valor_s = int(input(f"""
+        Você não pode sacar mais do que tem !
+        seu saldo é de............. R${saldoatual:.2f}
+        """))
+
+    if valor_s <=500:
+        saldo_atual(-valor_s)
+
+
+def depositar():
+    valor_d = int(input("Qual valor do deposito ?\n>>"))
+    while valor_d <0:            
+        valor_d = int(input(f"Valor R${valor_d:.2f} invalido, Informe um valor positivo !\n>>"))
+    
+    saldo_atual(valor_d)
+
+
+def saldo_atual(valor):
+    
+    if valor < 0:
+        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")        
+        acoes.append(f'Saque no valor de R${valor:.2f} realizado as {data}')
+    else:
+        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        acoes.append(f'Deposito de R${valor:.2f} realizado as {data}')
+    
+    saldo += valor
+    print(f'=========== {saldo}')
+    for acao in acoes:
+        print(acao)
+    print(f'saldo atual..... R${saldo:.2f}')
+    return saldo
+
 acoes = []
 saque = 3
 escolha = choice()
-while True:
+while escolha != 'q':
 
     if escolha == 's':
-        valor_s = int(input("Você pode sacar até R$500,00\n>>"))
-        while valor_s > 500:
-            valor_s = int(input(f"O valor R${valor_s:.2f} INVALIDO, maximo permitido 500,00\n>>"))
-
-        while valor_s > saldo:
-            valor_s = int(input(f"""
-            Você não pode sacar mais do que tem !
-            seu saldo é de............. R${saldo:.2f}
-            """))
-
-        if valor_s <=500:
-            data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            saldo -= valor_s
-            acoes.append(f'Saque no valor de R${valor_s:.2f} realizado as {data}')
-            
+        sacar()
         escolha = choice()
-        saque -=1
-        if saque == 0:
-            print('>> Limite de saque atingido <<')
-            break
 
 
     elif escolha == 'd':
-        valor_d = int(input("Qual valor do deposito ?\n>>"))
-        while valor_d <0:            
-            valor_d = int(input(f"Valor R${valor_d:.2f} invalido, Informe um valor positivo !\n>>"))
-        saldo += valor_d
-
-        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        acoes.append(f'Deposito de R${valor_d:.2f} realizado as {data}')
-
+        depositar()
         escolha = choice()
 
+        
     elif escolha == 'e':
-        for acao in acoes:
-            print(acao)
-        print(f'saldo atual..... R${saldo:.2f}')
+        saldo_atual()
         escolha = choice()
+
 
     elif escolha == 'q':
         break
