bdba14a46dcef0c385c321d10238fb594bef2297
Ronaldo Montan <ronaldogithub@gmail.com>
2023-04-03 22:50:53 -0300

    anotações de hoje

--- Formação Python Developer/mongoDB.py
+++ Formação Python Developer/mongoDB.py
@@ -10,11 +10,31 @@ https://pymongo.readthedocs.io/en/stable/atlas.html
 
 depois é preciso ter instalado build-essential:
 sudo apt install build-essential python3-dev
+
+
 """
 
 import pymongo as pym
+from pymongo import collation
+import datetime
 
-client = pym.MongoClient("mongodb+srv://ronaldo:<password>@cluster0.cg7kybh.mongodb.net/?retryWrites=true&w=majority")
-print(type(client))
-print('===')
-print(client)
+client = pym.MongoClient("mongodb+srv://momo:momo@cluster0.u6y9agk.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+collection = db.test_collection
+
+
+post = {
+    "author": "mike",
+    "text": "my first mongoDB application based on python",
+    "tags": ["mongodb", "python3", "pymongo"],
+    "date": datetime.datetime.now()
+}
+
+#preparando para submeter as informações
+posts = db.posts
+result = posts.insert_one(post)
+
+if result.acknowledged:
+    print("Inserido com sucesso. Id do registro:", result.inserted_id)
+else:
+    print("Ocorreu um erro ao inserir os dados.")
