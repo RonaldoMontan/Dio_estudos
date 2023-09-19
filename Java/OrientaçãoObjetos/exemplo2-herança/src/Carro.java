10929a191b09670c92fac50724a27142ca502266
Ronaldo Montan <ronaldogithub@gmail.com>
2023-09-19 00:40:05 -0300

    iniciando alguns testes com herança

---
+++ Java/OrientaçãoObjetos/exemplo2-herança/src/Carro.java
@@ -0,0 +1,8 @@
+public class Carros extends Veiculo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("nada");
+    
+    
+}
---
+++ Java/OrientaçãoObjetos/exemplo2-herança/src/Moto.java
@@ -0,0 +1,7 @@
+public class Moto  extends Veiculo{
+    
+    public static void main(String[] args) {
+        
+        Veiculo minhaMoto = new Veiculo(modelo: "Biz/ES", potencia:80);
+    }
+}
