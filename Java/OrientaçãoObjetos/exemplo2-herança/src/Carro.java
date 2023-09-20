766aef393f76d98a23775454a10257c0e09f00cf
Ronaldo Montan <ronaldogithub@gmail.com>
2023-09-19 23:44:46 -0300

    Trocando o nome da pasta

--- Java/OrientaçãoObjetos/exemplo2-herança/src/Carro.java
+++
@@ -1,8 +0,0 @@
-public class Carros extends Veiculo {
-
-    public static void main(String[] args) {
-        
-        System.out.println("nada");
-    
-    
-}
--- Java/OrientaçãoObjetos/exemplo2-herança/src/Moto.java
+++
@@ -1,7 +0,0 @@
-public class Moto  extends Veiculo{
-    
-    public static void main(String[] args) {
-        
-        Veiculo minhaMoto = new Veiculo(modelo: "Biz/ES", potencia:80);
-    }
-}
