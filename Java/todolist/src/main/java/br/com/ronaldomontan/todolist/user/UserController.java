155b4ce97ac0ed167d37f3a015f56b872a515a56
Ronaldo Montan <ronaldogithub@gmail.com>
2023-10-14 17:01:14 -0300

    Configurações do banco h2, inserindo registros e tratando com o spring boot

--- Java/todolist/src/main/java/br/com/ronaldomontan/todolist/user/UserController.java
+++ Java/todolist/src/main/java/br/com/ronaldomontan/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.ronaldomontan.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +23,23 @@ public class UserController {
      * PATCH - alterar somente uma parte da info/dado
      */
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
-        System.out.println(userModel.hashCode());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            System.out.println("Usuario já existe");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+
+        }
+        var userCreated = this.userRepository.save(userModel);
+        
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
