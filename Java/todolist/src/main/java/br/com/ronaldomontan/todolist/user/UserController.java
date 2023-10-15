26866864fb6824d927ef9dc0c5a84db09147bad0
Ronaldo Montan <ronaldogithub@gmail.com>
2023-10-14 21:58:52 -0300

    Criando endpoint task

--- Java/todolist/src/main/java/br/com/ronaldomontan/todolist/user/UserController.java
+++ Java/todolist/src/main/java/br/com/ronaldomontan/todolist/user/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 
 @RestController // retorno API
 @RequestMapping("/users")
@@ -35,11 +37,14 @@ public class UserController {
             System.out.println("Usuario já existe");
 
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
-
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+            .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
-        
-        
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
