8ba478b6348e7b836bd70c1f07213e46b4ed25a4
RonaldoMontan <ronaldogithub@gmail.com>
2024-08-07 00:41:46 -0300

    Alter language to english

--- Go_developer/desafio_1/main.go
+++ Go_developer/desafio_1/main.go
@@ -4,16 +4,14 @@ import "fmt"
 
 func KelvinToCelsius(kelvin float32) float32{
 	return kelvin - 273.15
-
 }
 
 func main() {
 	
-	var valor float32
-	fmt.Println("Conversão de Kelvin para Celsius.")
-	fmt.Scanf("%f", &valor)
+	var value float32
+	fmt.Println("Convert from Kelvin to Celsius.")
+	fmt.Scanf("%f", &value)
 
-	celsius := KelvinToCelsius(valor)
-	fmt.Println("O valor convertido é: ", celsius)
-		
+	celsius := KelvinToCelsius(value)
+	fmt.Println("The value converted is: ", celsius)		
 }
