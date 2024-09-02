47bdc9d6596ea96290c0b10ee5a14317eae81588
RonaldoMontan <ronaldogithub@gmail.com>
2024-09-01 21:51:37 -0300

    Configure instalator docker for machines

--- Docker-Fundamentals/lab-vagrand/instalar-docker.sh
+++ Docker-Fundamentals/lab-vagrand/instalar-docker.sh
@@ -1,2 +1,5 @@
- curl -fsSL https://get.docker.com -o get-docker.sh
- sudo sh get-docker.sh
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
